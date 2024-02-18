ec5a0dc73c8f57db484993ae8ae1364ff278264d
tomo-x7 <158121497+tomo-x7@users.noreply.github.com>
2024-02-18 11:16:11 +0900

    add

---
+++ background.js
@@ -0,0 +1,21 @@
+chrome.tabs.onActivated.addListener( (activeInfo)=> {
+  chrome.tabs.get(activeInfo.tabId, (tab)=> {
+    setAction(tab.url);
+  });
+});
+
+chrome.tabs.onUpdated.addListener((tabId, change, tab) => {
+  if (tab.active && change.url) {
+    setAction(change.url);
+  }
+});
+
+function setAction(url) {
+  // console.log(url);
+  const pattern = /^https:\/\/beta.sparebeat.com\/play\//;
+  if (pattern.test(url)) {
+    chrome.action.setPopup({ popup: "./popup.html"})
+  } else {
+    chrome.action.setPopup({ popup: "./errorpopup.html" });
+  }
+}
