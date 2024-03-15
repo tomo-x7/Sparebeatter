3c0b8ebd0aaa4ebad4c69ba6a1b7d9b204782227
tomo-x7 <158121497+tomo-x7@users.noreply.github.com>
2024-03-15 15:35:51 +0900

    update

--- website/sparebeatter/app/page.tsx
+++ website/sparebeatter/app/page.tsx
@@ -1,38 +1,36 @@
 import Image from "next/image";
 
 export default function Home() {
-  return (<div>test page</div>);
+	return (<div>test page</div>);
 }
 
-export async function generateMetadata(data) {
-  console.log(data)
-  return {
+export async function generateMetadata({searchParams}) {
+	let querys:string=''
+	for(const key in searchParams){
+		querys+=`${key}=${searchParams[key]}&`
+	}
+	console.log(querys)
+	return {
 		metadataBase: new URL("https://sparebeatter-git-dev-tomo-xs-projects.vercel.app/"),
 		title: 'sparebeatter',
-		description: description,
+		description: 'sparebeatのリザルトをツイート、Map Editorのキーコンフィグの変更などの便利機能詰め合わせ拡張機能',
 		openGraph: {
-			title: title,
-			description: description,
-			siteName: "所沢北高校物理部",
-			images: [
-				{
-					url: "https://tkbutsuribu.vercel.app/images/iconmain.svg",
-					width: 320,
-					height: 320,
-				},
-			],
-			type: "article",
+			title: 'sparebeatter',
+			description: 'sparebeatのリザルトをツイート、Map Editorのキーコンフィグの変更などの便利機能詰め合わせ拡張機能',
+			siteName: "sparebeatter",
+			type: "website",
+			images:
+			{url:`https://sparebeatter-git-dev-tomo-xs-projects.vercel.app/api/img?${querys}`}
 		},
 		icons: {
 			icon: "/favicon.ico",
 		},
-		manifest: "https://tkbutsuribu.verce.app/manifest.webmanifest",
 		twitter: {
 			card: "summary_large_image",
-			title: '',
-			description: description,
-			site: "@TK_physics_club",
-			image: ["https://tkbutsuribu.vercel.app/images/iconmain.svg"],
+			title: 'sparebeatter',
+			description: 'sparebeatのリザルトをツイート、Map Editorのキーコンフィグの変更などの便利機能詰め合わせ拡張機能',
+			site: "@tomo_x_79",
+			image: [`https://sparebeatter-git-dev-tomo-xs-projects.vercel.app/api/img?${querys}`],
 		},
 	};
 }
