d753033b4b520d50bcf26a7f4f8be8dbe103e2a6
tomo-x7 <158121497+tomo-x7@users.noreply.github.com>
2024-03-17 02:20:47 +0900

    fix API

--- website/sparebeatter/app/api/shorturl/route.tsx
+++ website/sparebeatter/app/api/shorturl/route.tsx
@@ -2,14 +2,13 @@ import { NextResponse, type NextRequest } from "next/server";
 
 export async function GET(req: NextRequest) {
 	const longurl = new URL(req.url).searchParams.get('url')
-	const token = "5muyBudWsWYqwPHOPEAKxCw0iBgv8wl3kOrYHA6zJflh7odf7Qy5kqhDJAU5";
 	const body = JSON.stringify({
 		url: longurl,
 		domain: "tinyurl.com",
 	});
 	const ret = await fetch("https://api.tinyurl.com/create", {
 		headers: {
-			Authorization: `Bearer ${process.env.TINYURL_TOKEN ?? token}`,
+			Authorization: `Bearer ${process.env.TINYURL_TOKEN}`,
 			"Content-Type": "application/json",
 		},
 		body: body,
