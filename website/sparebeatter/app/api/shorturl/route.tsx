e47b16c436d5eb133c766112ef0b3b7ccfc8896b
tomo-x7 <158121497+tomo-x7@users.noreply.github.com>
2024-03-16 16:58:14 +0000

    update api

---
+++ website/sparebeatter/app/api/shorturl/route.tsx
@@ -0,0 +1,22 @@
+import { NextResponse, type NextRequest } from "next/server";
+
+export async function POST(req: NextRequest) {
+	const longurl = (await req.json()).url;
+	const token = "5muyBudWsWYqwPHOPEAKxCw0iBgv8wl3kOrYHA6zJflh7odf7Qy5kqhDJAU5";
+	const body = JSON.stringify({
+		url: longurl,
+		domain: "tinyurl.com",
+	});
+	const ret = await fetch("https://api.tinyurl.com/create", {
+		headers: {
+			Authorization: `Bearer ${process.env.TINYURL_TOKEN ?? token}`,
+			"Content-Type": "application/json",
+		},
+		body: body,
+		method: "POST",
+	});
+	return NextResponse.json(
+		{ url: (await ret.json()).data.tiny_url },
+		{ status: ret.status },
+	);
+}
